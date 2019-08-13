698525e976b494c181a2f6355ea001dc32623652
joeran.kuschel <joeran.kuschel@open-xchange.com>
2019-08-13 13:50:13 +0200

    - Switched yes or no prompts to choice type - Lean package.json - Only install e2e packages if necessary

--- generators/app/index.js
+++ generators/app/index.js
@@ -15,13 +15,15 @@ module.exports = class OxUiModuleGenerator extends Generator {
             message: 'What do you want the name of your package to be?',
             default: this.appname
         }, {
+            type: 'confirm',
             name: 'translations',
-            message: 'Do you want to include translations into your package? (y|n)',
-            default: 'y'
+            message: 'Do you want to include translations into your package?',
+            default: true
         }, {
+            type: 'confirm',
             name: 'e2eTests',
-            message: 'Do you want to include e2e tests into your package? (y|n)',
-            default: 'y'
+            message: 'Do you want to include e2e tests into your package?',
+            default: true
         }]);
     }
 
@@ -34,12 +36,13 @@ module.exports = class OxUiModuleGenerator extends Generator {
         // Add apps folder
         mkdirp.sync('./apps');
         // Create ox.pot in i18n 
-        if (this.answers.translations === 'y') {
+        if (this.answers.translations === true) {
             this.fs.copy(this.templatePath('i18n/ox.pot'), this.destinationPath('i18n/ox.pot'));
         }
         // Create scaffolding for e2e 
-        if (this.answers.e2eTests === 'y') {
+        if (this.answers.e2eTests === true) {
             mkdirp.sync('./e2e/output');
+            this.npmInstall(['@open-xchange/codecept-helper', 'chai', 'codeceptjs', 'eslint-plugin-codeceptjs', 'selenium-standalone', 'webdriverio'], { 'save-dev': true });
             this.fs.copyTpl(this.templatePath('_codecept.conf.js'), this.destinationPath('codecept.conf.js'), { slugify, moduleName });
             this.fs.copy(this.templatePath('e2e/actor.js'), this.destinationPath('e2e/actor.js'));
             this.fs.copy(this.templatePath('e2e/helper.js'), this.destinationPath('e2e/helper.js'));
@@ -48,11 +51,12 @@ module.exports = class OxUiModuleGenerator extends Generator {
 
         this.fs.copy(this.templatePath('gitignore'), this.destinationPath('.gitignore'));
     }
-
     install() {
-        this.installDependencies();
+        return this.installDependencies();
+    }
+    end (){
         // Install selenium standalone
-        if (this.answers.e2eTests === 'y') this.spawnCommand('npx', ['selenium-standalone', 'install']);
+        if (this.answers.e2eTests === true) this.spawnCommand('npx', ['selenium-standalone', 'install']);
         return;
     }
 };
